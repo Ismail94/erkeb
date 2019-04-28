0c8a7ef118668bd55568e18e187b9c690b6a9e3b
ismailabes <ismail.abes@student.ehb.be>
2019-04-28 22:36:36 +0200

    Firebase toegevoegd aan het project

--- erkeb/AppDelegate.swift
+++ erkeb/AppDelegate.swift
@@ -7,6 +7,7 @@
 //  Test eerste commit
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
         containerVC = ContainerVC()
         window?.rootViewController = containerVC
         window?.makeKeyAndVisible()
