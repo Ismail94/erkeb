796f86c5d517e4811ee5fb276751153d313d3f5e
ismailabes <ismail.abes@student.ehb.be>
2019-04-28 21:28:40 +0200

    Nieuwe scherm aangemaakt: Launchscreen + animatie

--- erkeb/Controller/HomeVC.swift
+++ erkeb/Controller/HomeVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import RevealingSplashView
 
 class HomeVC: UIViewController, MKMapViewDelegate {
 
@@ -16,9 +17,22 @@ class HomeVC: UIViewController, MKMapViewDelegate {
     
     var delegate: CenterVCDelegate?
     
+    //Load image for launchscreen
+    
+    let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "LaunchScreenIcon")!, iconInitialSize: CGSize(width: 212, height: 254), backgroundImage: UIImage(named: "LaunchScreenBG")!)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        
+        //Set the launchscreen animation
+        self.view.addSubview(revealingSplashView)
+        revealingSplashView.animationType = SplashAnimationType.squeezeAndZoomOut
+        revealingSplashView.startAnimation()
+        
+        //Call to stop the animation
+        revealingSplashView.heartAttack = true
        
     }
     
