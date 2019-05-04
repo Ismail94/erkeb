0d0bc7e61411ca833624029b14dc3c91746c3729
ismailabes <ismail.abes@student.ehb.be>
2019-05-04 23:40:33 +0200

    Nieuwe subclass aangemaakt voor de driver annotation

---
+++ erkeb/View/DriverAnnotation.swift
@@ -0,0 +1,30 @@
+//
+//  DriverAnnotation.swift
+//  erkeb
+//
+//  Created by Ismail on 04/05/2019.
+//  Copyright © 2019 Ismail Abes. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class DriverAnnotation: NSObject, MKAnnotation{
+    var coordinate: CLLocationCoordinate2D
+    var key: String
+    
+    init(coordinate: CLLocationCoordinate2D, withKey key: String){
+        self.coordinate = coordinate
+        self.key = key
+        super.init()
+    }
+    
+    func update(annotationPostition annotation: DriverAnnotation, withCoordinate coordinate: CLLocationCoordinate2D){
+        var location = self.coordinate
+        location.latitude = coordinate.latitude
+        location.longitude = coordinate.longitude
+        UIView.animate(withDuration: 0.2) {
+            self.coordinate = location
+        }
+    }
+}
