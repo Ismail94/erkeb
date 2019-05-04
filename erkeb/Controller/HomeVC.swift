d9b3c17a86f4f571809a8e064f53dd32ec3084fe
ismailabes <ismail.abes@student.ehb.be>
2019-05-04 21:40:41 +0200

    Start mapkit:  het instellen van de  "Location Services and Permissions" en de center map button

--- erkeb/Controller/HomeVC.swift
+++ erkeb/Controller/HomeVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 import RevealingSplashView
 
 class HomeVC: UIViewController, MKMapViewDelegate {
@@ -17,6 +18,10 @@ class HomeVC: UIViewController, MKMapViewDelegate {
     
     var delegate: CenterVCDelegate?
     
+    var manager : CLLocationManager?
+    
+    var regionRadius: CLLocationDistance = 1000
+    
     //Load image for launchscreen
     
     let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "LaunchScreenIcon")!, iconInitialSize: CGSize(width: 212, height: 254), backgroundImage: UIImage(named: "LaunchScreenBG")!)
@@ -24,7 +29,16 @@ class HomeVC: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        
+        checkLocationAuthStatus()
+        
         mapView.delegate = self
+        
+        centerMapOnUserLocation()
         
         //Set the launchscreen animation
         self.view.addSubview(revealingSplashView)
@@ -33,17 +47,45 @@ class HomeVC: UIViewController, MKMapViewDelegate {
         
         //Call to stop the animation
         revealingSplashView.heartAttack = true
-       
+    }
+    
+    //hier kijk ik als er al authorisatie zo niet dan vraag ik het terug
+    func checkLocationAuthStatus(){
+        if CLLocationManager.authorizationStatus() == .authorizedAlways{
+            manager?.startUpdatingLocation()
+        } else{
+            manager?.requestAlwaysAuthorization()
+        }
+    }
+    
+    //Kaart centereren op de location van de gebruiker
+    func centerMapOnUserLocation(){
+        let coordinateRegion = MKCoordinateRegion(center: mapView.userLocation.coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
     
     
     @IBAction func boekEenRitBtnWasPressed(_ sender: Any) {
         boekEenRitBtn.animateButton(shouldLoad: true, withMessage: nil)
     }
+   
+    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        centerMapOnUserLocation()
+    }
     
     @IBAction func menuBtnWasPressed(_ sender: Any) {
         delegate?.toggleMenuLeftPanel()
     }
-    
+}
+
+extension HomeVC: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthStatus()
+        if status == .authorizedAlways {
+            mapView.showsUserLocation = true
+            // als de gerbuiker beweegt dan gaat de pin in het midden van de scherm blijven en kaart zal dan volgen
+            mapView.userTrackingMode =  .follow
+        }
+    }
 }
 
